8e8e2903a1057601b5e94488c8e12be0abd59145
jiang <jiang5630@outlook.com>
2025-07-30 14:55:24 +0800

    Update AuthServer

--- AuthServer/go.mod
+++ AuthServer/go.mod
@@ -4,6 +4,7 @@ go 1.23.4
 
 require (
 	github.com/bwmarrin/snowflake v0.3.0
+	github.com/eclipse/paho.mqtt.golang v1.5.0
 	github.com/gogf/gf/contrib/drivers/pgsql/v2 v2.9.0
 	github.com/gogf/gf/v2 v2.9.0
 
@@ -25,8 +26,6 @@ require (
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
-	github.com/olekukonko/errors v1.1.0 // indirect
-	github.com/olekukonko/ll v0.0.9 // indirect
 	github.com/olekukonko/tablewriter v1.0.9 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
@@ -35,6 +34,7 @@ require (
 	go.opentelemetry.io/otel/sdk v1.37.0 // indirect
 	go.opentelemetry.io/otel/trace v1.37.0 // indirect
 	golang.org/x/net v0.42.0 // indirect
+	golang.org/x/sync v0.16.0 // indirect
 	golang.org/x/sys v0.34.0 // indirect
 	golang.org/x/text v0.27.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- AuthServer/main.go
+++ AuthServer/main.go
@@ -13,6 +13,7 @@ import (
 	NTPack "AuthServer/App"
 	"encoding/json"
 	"fmt"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 	_ "github.com/gogf/gf/contrib/drivers/pgsql/v2"
 	"github.com/gogf/gf/v2/encoding/gjson"
 	"github.com/gogf/gf/v2/frame/g"
@@ -28,6 +29,9 @@ var (
 	ComponentId  string
 	StartTime    time.Time
 	ServerNodeId int64
+
+	// 全局对象 MQTT客户端
+	MqttClient mqtt.Client
 )
 
 func main() {
@@ -42,9 +46,17 @@ func main() {
 	ComponentId = mComponentId.String()
 	ServerNodeId = mServerNodeId.Int64()
 	StartTime = time.Now()
+	//生成ComponentHeader数据
+	mHeader := NTPack.NewComponentHeader(ServerNodeId)
 	//创建MQTT客户端
-
-	//TODO 检查消息服务器是否可用，如果不可用则启动消息服务器
+	InitMqttClient(*mHeader)
+	fmt.Println("Testing message server...")
+	//TODO 检查消息服务器是否可用，如果不可用则退出进程
+	if token := MqttClient.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+		return
+	}
+	defer MqttClient.Disconnect(250) // 优雅断开连接，等待250ms处理剩余消息
 
 	//设置Http路径
 	s := g.Server()
@@ -53,8 +65,6 @@ func main() {
 	mGroup.GET("/list", GetList)   //返回已注册程序列表
 	mGroup.POST("/reg", PostReg)   //注册新设备
 
-	//生成ComponentHeader数据
-	mHeader := NTPack.NewComponentHeader(ServerNodeId)
 	//清理Components表
 	_, _ = g.DB().Model("Components").Delete("1=1")
 	//将自身Header数据插入Components表
@@ -64,6 +74,51 @@ func main() {
 	//TODO 广播"重新注册"消息，预防因AuthServer中途退出或重启而丢失设备。设备自行发布上线消息。
 
 	s.Run()
+}
+
+func MqttOnConnect(client mqtt.Client) {
+	fmt.Println("Connected")
+	//订阅频道
+	MqttClient.Subscribe("ntcb/#", 0, nil)
+}
+
+func MqttOnLostConnect(client mqtt.Client, err error) {
+	fmt.Println("LostConnect")
+}
+
+func MqttOnMessage(client mqtt.Client, Message mqtt.Message) {
+	fmt.Println("OnMessage___________________")
+	fmt.Println(Message.Topic())
+	fmt.Println(string(Message.Payload()))
+}
+
+// InitMqttClient 初始化Mqtt客户端
+func InitMqttClient(AComponent NTPack.TCBComponentHeader) {
+	//从Config文件读取参数
+	mCtx := gctx.New()
+	mReconnectDuration, _ := g.Config().Get(mCtx, "ntcb.reconnectDuration") //最大重新连接时间
+	mBroker, _ := g.Config().Get(mCtx, "ntcb.broker")                       //MQTT服务器地址
+	mBrokerUser, _ := g.Config().Get(mCtx, "ntcb.brokerUser")               //MQTT服务器用户
+	mBrokerPassword, _ := g.Config().Get(mCtx, "ntcb.brokerPassword")       //MQTT服务器用户密码
+
+	opts := mqtt.NewClientOptions()
+	opts.SetAutoReconnect(true)
+	opts.SetConnectRetry(true)
+	opts.SetConnectRetryInterval(30 * time.Second) //设置超时重试时间为15秒
+	opts.SetKeepAlive(15 * time.Second)            //设置心跳为15秒
+	//客户端ID为Component，本地IP和进程PID的组合
+	opts.SetClientID(fmt.Sprintf("%s_%s_%d", AComponent.ComponentID, AComponent.LocalIP, AComponent.Pid))
+	opts.SetMaxReconnectInterval(time.Duration(mReconnectDuration.Int()) * time.Second)
+	opts.AddBroker(mBroker.String())
+	//设置Mqtt客户端事件
+	opts.SetOnConnectHandler(MqttOnConnect)
+	opts.SetConnectionLostHandler(MqttOnLostConnect)
+	opts.SetDefaultPublishHandler(MqttOnMessage)
+	//设置Mqtt客户端用户名和密码
+	opts.SetUsername(mBrokerUser.String())
+	opts.SetPassword(mBrokerPassword.String())
+	// 创建客户端
+	MqttClient = mqtt.NewClient(opts)
 }
 
 // GetList 返回已注册程序列表
