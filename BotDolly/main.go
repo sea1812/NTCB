572dfb4c012bd1f9676173cb94aef08fc2bf232c
jiang <jiang5630@outlook.com>
2025-08-03 17:04:53 +0800

    Create BotDolly

---
+++ BotDolly/main.go
@@ -0,0 +1,27 @@
+/**--------------------------------------------------------
+ * Author: jiang5630@outlook.com 2025-08-03
+ * Description:
+ * This file is part of the BotDolly project.
+ * --------------------------------------------------------
+ * 作者：jiang5630@outlook.com  2025年08月03日
+ * 描述：第一个Bot Dolly，用于测试模版
+ --------------------------------------------------------*/
+
+package main
+
+func main() {
+	//读取Config中的参数设置
+	//生成ComponentHeader
+	//向Auth Server申请注册
+	//注册成功，创建MQTT客户端
+	//连接到Broker
+	//订阅信道
+	//发布上线通报
+	//设置定时任务，发送STAT广播，默认为每10分钟
+	//启动定时任务
+	//广播上线
+	//如果是自动执行JOB，则启动JOB
+	//发送JOB启动消息
+	//JOB完成，发送JOB DONE消息
+	//如果设置了AutoShutdown则退出，否则等待信号
+}
